44fb38e249b538e115f1d20dcd86c35205c4bf7f
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-12 21:30:36 -0400

    Flask imports in app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.exceptions import Unauthorized
+
+from models import connect_db, db, User, Feedback
+from forms import RegisterForm, LoginForm, FeedbackForm, DeleteForm
+
+app = Flask(__name__)
