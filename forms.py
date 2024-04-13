dad99119d61a276080f15c92d0898f3a6bd2d844
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-12 21:43:45 -0400

    register form

--- forms.py
+++ forms.py
@@ -13,3 +13,27 @@ class LoginForm(FlaskForm):
         "Password",
         validators=[InputRequired(), Length(min=6, max=55)],
     )
+
+class RegisterForm(FlaskForm):
+    """User registration form."""
+
+    username = StringField(
+        "Username",
+        validators=[InputRequired(), Length(min=1, max=20)],
+    )
+    password = PasswordField(
+        "Password",
+        validators=[InputRequired(), Length(min=6, max=55)],
+    )
+    email = StringField(
+        "Email",
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+    first_name = StringField(
+        "First Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
+    last_name = StringField(
+        "Last Name",
+        validators=[InputRequired(), Length(max=30)],
+    )
