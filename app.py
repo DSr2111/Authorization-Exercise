22e610431dc2588cb03de898b2da9ac9df15c048
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-04-12 21:30:56 -0400

    app.config code

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ from models import connect_db, db, User, Feedback
 from forms import RegisterForm, LoginForm, FeedbackForm, DeleteForm
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask-feedback"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "shhhhh"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
